056b289700f43546b61f1ed918cb003274961859
Walter Zhang <walterzhang@192-168-1-105.tpgi.com.au>
2021-04-27 23:24:27 +1000

    front end arch:

---
+++ frontend/client/app.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+}
