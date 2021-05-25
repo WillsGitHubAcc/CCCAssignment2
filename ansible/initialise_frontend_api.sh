5121d1e4d6e95e1a550e1d05660312737cb73cef
William Dean <wdean@student.unimelb.edu.au>
2021-05-26 00:58:33 +1000

    separate out proxies logic, add frontend_api spooling, add tweet_analysis spooling

---
+++ ansible/initialise_frontend_api.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+#. ./unimelb-comp90024-2021-grp-52-openrc.sh; ansible-playbook mrc.yaml
+# ansible-playbook --vault-password-file passwords.txt generate_vms.yaml
+ansible-playbook --vault-password-file passwords.txt frontend_api_initialisation.yaml
