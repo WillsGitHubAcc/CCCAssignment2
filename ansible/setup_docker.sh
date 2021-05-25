cefa2b00adb98d8ce7007283d14d3a20a8769aba
William Dean <wdean@student.unimelb.edu.au>
2021-05-26 02:04:39 +1000

    Add team header to files

--- ansible/setup_docker.sh
+++ ansible/setup_docker.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/bash
 
+# COMP90024 2021 Semester 2 Assignment 2
+# Group 52
+# William Lazarus Kevin Dean 834444 Melbourne, Australia
+# Kenneth Huynh 992680 Melbourne, Australia
+# Joel Kenna 995401 Melbourne, Australia
+# Quinten van der Leest 1135216 Melbourne, Australia
+# Walter Zhang 761994 Melbourne, Australia
+
 #. ./unimelb-comp90024-2021-grp-52-openrc.sh; ansible-playbook mrc.yaml
 # ansible-playbook --vault-password-file passwords.txt generate_vms.yaml
 ansible-playbook --vault-password-file passwords.txt docker_setup.yaml
