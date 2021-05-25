ccf0a7e0b8be00a603b361814baa7ead371c4410
Walter Zhang <walterzhang@ravpn-266-2-student-ten-8-91-84.uniaccess.unimelb.edu.au>
2021-05-26 02:45:55 +1000

    added sleep

--- client/src/extraGraph.js
+++ client/src/extraGraph.js
@@ -3,6 +3,8 @@ import Plotly from "plotly.js-basic-dist";
 import createPlotlyComponent from "react-plotly.js/factory";
 import * as aurin_pets from "./aurin_pets.json";
 import * as aurin_elect from "./aurin_elect.json";
+import * as aurin_sleep from "./data/Victoria_sleep_study.json";
+
 
 export default function SpacingGrid(props) {
   var scenario = props.scenario;
@@ -17,6 +19,11 @@ if(scenario==="pet"){
 if(scenario==="elect"){
     return (
       <Plot data={aurin_elect.default.data} layout={aurin_elect.default.layout} frames={aurin_elect.default.frames} />
+    );
+  }
+if(scenario==="sleep"){
+    return (
+      <Plot data={aurin_sleep.default.data} layout={aurin_sleep.default.layout} frames={aurin_sleep.default.frames} />
     );
   }
 
