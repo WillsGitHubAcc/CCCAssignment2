8dd0cfb559ff2dcbf95bf7323a84620abca8ea62
Kenneth H <40946760+k-huynh@users.noreply.github.com>
2021-05-27 11:15:19 +1000

    Update full_deploy.sh

--- ansible/full_deploy.sh
+++ ansible/full_deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-sh ./make_vms.sh
-sh ./clone_gits.sh
-sh ./setup_docker.sh
-sh ./initialise_database.sh
-sh ./load_view.sh
-sh ./initialise_harvesters.sh
-sh ./initialise_tweet_analysis.sh
-sh ./initialise_frontend.sh
-sh ./initialise_frontend_api.sh
+./make_vms.sh
+./clone_gits.sh
+./setup_docker.sh
+./initialise_database.sh
+./load_view.sh
+./initialise_harvesters.sh
+./initialise_tweet_analysis.sh
+./initialise_frontend.sh
+./initialise_frontend_api.sh
